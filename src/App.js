e92ade2a8d051454a22ab1ea14cd1ac5ad51b3c3
Marge <marge.consunji@gmail.com>
2022-06-01 13:50:27 +0800

    Import InputForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import InputForm from './components/InputForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
+      <InputForm />
     </div>
   );
 }
