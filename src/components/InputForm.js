c810cedaa2946f41450cc4f39a7ad2b557083334
Marge <marge.consunji@gmail.com>
2022-06-01 13:50:09 +0800

    Make add transaction form

---
+++ src/components/InputForm.js
@@ -0,0 +1,22 @@
+const InputForm = () => {
+  const submitTransaction = (e) => {
+    e.preventDefault();
+    console.log('firing');
+  };
+
+  return (
+    <form onSubmit={submitTransaction}>
+      <label>
+        Transaction:
+        <input type="text" name="transaction" />
+      </label>
+      <label>
+        Amount:
+        <input type="text" name="transaction" />
+      </label>
+      <input type="submit" value="Add new transaction" />
+    </form>
+  );
+};
+
+export default InputForm;
