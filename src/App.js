8027993cad6f1d93acf62ad626423d8a9a9dc140
Marge <marge.consunji@gmail.com>
2022-06-01 15:47:43 +0800

    Change classname

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Balance from './components/Balance';
 import TransactionForm from './components/TransactionForm';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="tracker-container">
       <h1>Expense Tracker</h1>
+      <Balance />
       <TransactionForm />
+      <TransactionList />
     </div>
   );
 }
