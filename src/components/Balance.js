1fb59fd927a8b05369519ddab17826eef931a3d6
Marge <marge.consunji@gmail.com>
2022-06-02 10:24:23 +0800

    Make base component for balance

---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+const Balance = () => {
+  return (
+    <section>
+      <div>Balance</div>
+      <div>Total Income:</div>
+      <div>Total Expenses:</div>
+    </section>
+  );
+};
+
+export default Balance;
