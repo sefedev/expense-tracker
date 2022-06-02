9b58ee2ba64f31fc10eed80ee1556bd6f0543ac0
Marge <marge.consunji@gmail.com>
2022-06-02 10:25:01 +0800

    Make base file for list of transactions

---
+++ src/components/TransactionList.js
@@ -0,0 +1,12 @@
+const TransactionList = () => {
+  return (
+    <section>
+      <h2>All Transactions</h2>
+      <ul>
+        <li>item goes here</li>
+      </ul>
+    </section>
+  );
+};
+
+export default TransactionList;
