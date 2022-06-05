c541f3c751e5116546d32770ca9bfbf97964f076
Marge <marge.consunji@gmail.com>
2022-06-05 16:38:25 +0800

    Add edit function

--- src/context/context.js
+++ src/context/context.js
@@ -15,12 +15,25 @@ const TransactionProvider = ({ children }) => {
     );
   };
 
+  const editTransaction = (newTransaction, idToEdit) => {
+    console.log(newTransaction);
+    const updatedTransactions = transactions.map((transaction) => {
+      if (transaction.id === idToEdit) {
+        transaction.transactionName = newTransaction.transactionName;
+        transaction.amount = newTransaction.amount;
+      }
+      return transaction;
+    });
+    setTransactions(updatedTransactions);
+  };
+
   return (
     <TransactionContext.Provider
       value={{
         state: transactions,
         add: addTransaction,
         remove: removeTransaction,
+        edit: editTransaction,
       }}
     >
       {children}
