601dbd2bbbec6da283fb0f44147784df79a238a9
Marge <marge.consunji@gmail.com>
2022-06-13 15:27:22 +0800

    Add Sefe's base code for add transaction form

---
+++ src/components/TransactionForm.js
@@ -0,0 +1,80 @@
+import React, { useContext, useState } from 'react';
+import { TransactionContext } from 'context/context';
+
+const TransactionForm = (props) => {
+  const [newTransaction, setNewTransaction] = useState('');
+  const [newAmount, setNewAmount] = useState('');
+
+  // const addTransactionRef= useRef()
+  // const addAmountRef = useRef()
+
+  const { onAdd, onOpenModal } = useContext(TransactionContext);
+
+  let transaction = {
+    id: Math.floor(Math.random() * 1000),
+    transactionName: newTransaction,
+    amount: +newAmount,
+    type: null,
+  };
+
+  const transactionHandler = (e) => {
+    setNewTransaction(e.target.value);
+  };
+
+  const amountHandler = (e) => {
+    setNewAmount(e.target.value);
+  };
+
+  const addIncomeHandler = (e) => {
+    e.preventDefault();
+    const newIncome = { ...transaction, type: 'income' };
+    onAdd(newIncome);
+    setNewTransaction('');
+    setNewAmount('');
+    onOpenModal(false);
+  };
+
+  const addExpenseHandler = (e) => {
+    e.preventDefault();
+    const newExpense = { ...transaction, type: 'expense' };
+    onAdd(newExpense);
+    setNewTransaction('');
+    setNewAmount('');
+    onOpenModal(false);
+  };
+
+  //  console.log(addTransactionRef)
+
+  return (
+    <form>
+      <div className="form_control">
+        <label htmlFor="transaction">Add Transaction</label>
+        <input
+          type="text"
+          className="input_class"
+          onChange={transactionHandler}
+          value={newTransaction}
+        />
+      </div>
+      <div className="form_control">
+        <label htmlFor="amount">Amount</label>
+        <input
+          type="number"
+          className="input_class"
+          onChange={amountHandler}
+          value={newAmount}
+        />
+      </div>
+      <div className="form_control">
+        <button type="submit" onClick={addIncomeHandler}>
+          Add Income
+        </button>
+        <button type="submit" onClick={addExpenseHandler}>
+          Add Expense
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default TransactionForm;
