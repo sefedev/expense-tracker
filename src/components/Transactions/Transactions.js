9f1f76ac028b3750804345617b2432be222c1094
Marge <marge.consunji@gmail.com>
2022-06-15 08:47:41 +0800

    Make new EmptyTransaction component
    - Import image file
    - Set EmptyTransaction component if globalstate is empty

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,13 +1,34 @@
 import { useContext } from 'react';
 import { TransactionContext } from 'context/context';
 import Transaction from '../Transaction/Transaction';
+import emptyImg from 'assets/img/sammy-money-tree-1.png';
+
+const EmptyTransactions = () => {
+  return (
+    <div className="mt-20">
+      <img
+        src={emptyImg}
+        className="max-w-[15rem] mx-auto"
+        alt="Illustration of person watering a money tree"
+      />
+      <p className="text-center text-gray">
+        Looks like you haven’t added any transactions yet.
+        <br /> Click the ‘Add’ button to add a transaction.
+      </p>
+    </div>
+  );
+};
 
 const Transactions = () => {
   const { globalState } = useContext(TransactionContext);
   return (
     <section>
-      <h2>Transactions</h2>
-      {globalState.length === 0 && <p>No transactions to Show!</p>}
+      <div className="flex items-center place-content-between">
+        <h2 className="text-bold text-blue-dark">Transactions</h2>
+        Button goes here
+      </div>
+
+      {globalState.length === 0 && <EmptyTransactions />}
       <ul>
         {globalState.map((transaction) => (
           <Transaction key={transaction.id} {...transaction} />
