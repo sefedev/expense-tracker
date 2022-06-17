480eda692cc5ab5c7aa830bd42d674f7bc34a151
Marge <marge.consunji@gmail.com>
2022-06-17 16:55:17 +0800

    Add transaction details and styling

--- src/routes/transactionDetail.js
+++ src/routes/transactionDetail.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { TransactionContext, TransactionProvider } from 'context/context';
+import { Link, useParams } from 'react-router-dom';
 
 const TransactionDetail = () => {
   let params = useParams();
-  return <h2>Transaction Name</h2>;
+  const { globalState } = useContext(TransactionContext);
+  const getDetails = (id) => {
+    return globalState.find(
+      (transaction) => transaction.id === parseInt(params.id)
+    );
+  };
+  let { transactionName, amount, type } = getDetails(params.id);
+  return (
+    <TransactionProvider>
+      <main className="transactions-container">
+        <Link to="/">
+          <div>
+            <span className="text-sm font-bold text-gray">Go Back</span>
+          </div>
+        </Link>
+        <div className="mb-7">
+          <h1>Transaction Details</h1>
+        </div>
+        <div className="bg-white px-8 py-8 rounded-md space-y-4">
+          <div>
+            <p className="text-gray text-sm mb-1">Transaction Name</p>
+            <p className="font-bold">{transactionName}</p>
+          </div>
+          <div>
+            <p className="text-gray text-sm mb-1">Amount</p>
+            <p className="font-bold">{amount}</p>
+          </div>
+          <div>
+            <p className="text-gray text-sm mb-1">Transaction Type</p>
+            <p
+              className={`font-bold capitalize ${
+                type === 'expense' ? 'text-salmon' : 'text-green'
+              }`}
+            >
+              {type}
+            </p>
+          </div>
+        </div>
+      </main>
+    </TransactionProvider>
+  );
 };
 
 export default TransactionDetail;
