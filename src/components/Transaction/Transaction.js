047059f9093868a453023618de294287fbcc20d4
Marge <marge.consunji@gmail.com>
2022-06-17 16:54:58 +0800

    Remove useContext import

--- src/components/Transaction/Transaction.js
+++ src/components/Transaction/Transaction.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { ChevronRightIcon } from '@heroicons/react/solid';
 
