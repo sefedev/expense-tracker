b4fb17d8103d88511f3da862e601d33ba38c08e2
Marge <marge.consunji@gmail.com>
2022-06-17 16:42:15 +0800

    Remove context provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import { TransactionProvider } from 'context/context';
 import DisplayBalance from 'components/DisplayBalance/DisplayBalance';
 import TransactionInput from 'components/TransactionInput/TransactionInput';
 import Transactions from 'components/Transactions/Transactions';
 
 function App() {
   return (
-    <TransactionProvider>
-      <main className="transactions-container">
-        <Outlet />
-        <DisplayBalance />
-        <TransactionInput />
-        <Transactions />
-      </main>
-    </TransactionProvider>
+    <main className="transactions-container">
+      <Outlet />
+      <DisplayBalance />
+      <TransactionInput />
+      <Transactions />
+    </main>
   );
 }
 
