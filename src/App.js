2fb234933d403607d33c3ecbf08cd8911e2f88d3
Marge <marge.consunji@gmail.com>
2022-06-17 17:47:27 +0800

    Remove Outlet import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Outlet } from 'react-router-dom';
 import DisplayBalance from 'components/DisplayBalance/DisplayBalance';
 import TransactionInput from 'components/TransactionInput/TransactionInput';
 import Transactions from 'components/Transactions/Transactions';
@@ -6,7 +5,6 @@ import Transactions from 'components/Transactions/Transactions';
 function App() {
   return (
     <main className="transactions-container">
-      <Outlet />
       <DisplayBalance />
       <TransactionInput />
       <Transactions />
