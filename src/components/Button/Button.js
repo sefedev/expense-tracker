19d5b2ed2806adb2eb3c8a1766cf3a62b6cb9209
Marge <marge.consunji@gmail.com>
2022-06-21 14:48:25 +0800

    Add basic styling for base Button
    - Add styling to match Figma design
    - Default colour is blue
    - Add padding (horizontal and vertical)
    - Add text styling and make button pill shaped

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,12 @@
-const Button = ({ type, children, onClick }) => {
-  return <button>test</button>;
+const Button = ({ buttonType, children, onClick }) => {
+  return (
+    <button
+      className={`bg-blue py-2 px-7 font-bold text-white rounded-full`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
