1beeead1730bb27211028a9396e8bc52c2e78264
Marge <marge.consunji@gmail.com>
2022-06-21 14:20:53 +0800

    Export Primary button story
    - Test run primary button story for Button component

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,17 @@
+import Button from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
