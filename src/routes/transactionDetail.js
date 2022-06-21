155daf8ef4931c75965004e4a04e68cc1cb2a8a7
Marge <marge.consunji@gmail.com>
2022-06-21 12:28:13 +0800

    Remove redundant Transaction Provider

--- src/routes/transactionDetail.js
+++ src/routes/transactionDetail.js
@@ -13,42 +13,40 @@ const TransactionDetail = () => {
   };
   let { transactionName, amount, type } = getDetails(params.id);
   return (
-    <TransactionProvider>
-      <main className="transactions-container">
-        <Link to="/" className=" mb-10">
-          <div className="group inline-flex items-center hover:text-gray-dark">
-            <ChevronLeftIcon className="h-11 w-6 text-gray mr-1  group-hover:text-gray-dark" />
-            <span className="text-sm font-bold text-gray  group-hover:text-gray-dark">
-              Go Back
-            </span>
-          </div>
-        </Link>
+    <main className="transactions-container">
+      <Link to="/" className=" mb-10">
+        <div className="group inline-flex items-center hover:text-gray-dark">
+          <ChevronLeftIcon className="h-11 w-6 text-gray mr-1  group-hover:text-gray-dark" />
+          <span className="text-sm font-bold text-gray  group-hover:text-gray-dark">
+            Go Back
+          </span>
+        </div>
+      </Link>
 
-        <div className="mb-7">
-          <h1>Transaction Details</h1>
+      <div className="mb-7">
+        <h1>Transaction Details</h1>
+      </div>
+      <div className="bg-white px-8 py-10 rounded-md space-y-5">
+        <div>
+          <p className="text-gray text-sm mb-1">Transaction Name</p>
+          <p className="font-bold">{transactionName}</p>
         </div>
-        <div className="bg-white px-8 py-10 rounded-md space-y-5">
-          <div>
-            <p className="text-gray text-sm mb-1">Transaction Name</p>
-            <p className="font-bold">{transactionName}</p>
-          </div>
-          <div>
-            <p className="text-gray text-sm mb-1">Amount</p>
-            <p className="font-bold">{amount}</p>
-          </div>
-          <div>
-            <p className="text-gray text-sm mb-1">Transaction Type</p>
-            <p
-              className={`font-bold capitalize ${
-                type === 'expense' ? 'text-salmon' : 'text-green'
-              }`}
-            >
-              {type}
-            </p>
-          </div>
+        <div>
+          <p className="text-gray text-sm mb-1">Amount</p>
+          <p className="font-bold">{amount}</p>
         </div>
-      </main>
-    </TransactionProvider>
+        <div>
+          <p className="text-gray text-sm mb-1">Transaction Type</p>
+          <p
+            className={`font-bold capitalize ${
+              type === 'expense' ? 'text-salmon' : 'text-green'
+            }`}
+          >
+            {type}
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
