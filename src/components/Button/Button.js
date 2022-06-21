e7cab5193903046ded3c9c6c464893ca73102bd4
Marge <marge.consunji@gmail.com>
2022-06-21 15:01:58 +0800

    Add button variant styling
    - Using classnames, reorganise utility classes
    - Set background colours for cancel and primary button types

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,15 @@
+import classnames from 'classnames';
+
 const Button = ({ buttonType, children, onClick }) => {
   return (
     <button
-      className={`bg-blue py-2 px-7 font-bold text-white rounded-full`}
+      className={classnames(
+        buttonType === 'primary' && 'bg-blue',
+        buttonType === 'cancel' && 'bg-gray',
+        'py-2 px-7',
+        'font-bold text-white',
+        'rounded-full'
+      )}
       onClick={onClick}
     >
       {children}
