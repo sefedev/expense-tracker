39b0b22a34d12c201c72febad8f0c9ea8586430c
Marge <marge.consunji@gmail.com>
2022-06-21 15:04:36 +0800

    Set bg-color for Delete button variant
    - Use classnames and Tailwind to conditionally set bg colour to salmon if buttonType is 'delete'

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,6 +6,7 @@ const Button = ({ buttonType, children, onClick }) => {
       className={classnames(
         buttonType === 'primary' && 'bg-blue',
         buttonType === 'cancel' && 'bg-gray',
+        buttonType === 'delete' && 'bg-salmon',
         'py-2 px-7',
         'font-bold text-white',
         'rounded-full'
