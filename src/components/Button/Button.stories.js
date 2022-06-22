35ad64374d5e51df4e5a5ccfbf257061d03d6451
Marge <marge.consunji@gmail.com>
2022-06-22 11:58:18 +0800

    Add descriptions to each story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,7 +4,10 @@ export default {
   title: 'Button',
   component: Button,
   argTypes: {
+    description:
+      'This is a basic button component used in the expense tracker app',
     buttonType: { control: 'color' },
+    onClick: { action: 'clicked' },
   },
 };
 
@@ -14,6 +17,7 @@ export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
   label: 'Button',
+  description: 'This is the base button style used for adding and editing.',
   buttonType: 'primary',
   children: 'Primary button',
 };
@@ -22,6 +26,7 @@ export const Cancel = Template.bind({});
 Cancel.args = {
   primary: true,
   label: 'Button',
+  description: 'This is used for cancelling out of an action.',
   buttonType: 'cancel',
   children: 'Cancel',
 };
@@ -30,6 +35,7 @@ export const Delete = Template.bind({});
 Delete.args = {
   primary: true,
   label: 'Button',
+  description: 'This is used to delete a transaction and confirm deletion.',
   buttonType: 'delete',
   children: 'Delete',
 };
