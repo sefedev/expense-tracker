53fb47074f48a74fe360bb6eade3d6d5e923ea9c
Marge <marge.consunji@gmail.com>
2022-06-22 12:27:09 +0800

    Add extra styling to hover and padding
    - Horizontal padding is now different depending on the buttonType value
    - addNew button includes an SVG icon, so padding has been adjusted accordingly for that
    - hover state bg colours added

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,10 +4,12 @@ const Button = ({ buttonType, children, onClick }) => {
   return (
     <button
       className={classnames(
-        buttonType === 'primary' && 'bg-blue',
-        buttonType === 'cancel' && 'bg-gray',
-        buttonType === 'delete' && 'bg-salmon',
-        'py-2 px-7',
+        buttonType === 'primary' && 'bg-blue px-7 hover:bg-blue-dark-saturated',
+        buttonType === 'cancel' && 'bg-gray px-7 hover:bg-gray-dark',
+        buttonType === 'delete' && 'bg-salmon px-7 hover:bg-red',
+        buttonType === 'addNew' &&
+          'bg-blue pl-2 pr-4 hover:bg-blue-dark-saturated',
+        'py-2',
         'font-bold text-white',
         'rounded-full'
       )}
